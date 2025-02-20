2d0500049dfebc251dffc2d835e874c3abb99062
Giovanna Mastelari <gimastelari@gmail.com>
2025-02-19 19:37:08 -0500

    New additions to the app

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
+def home():
+    return render_template('index.html')
+
 def hello_world():
     return "Hello, World!"
 
